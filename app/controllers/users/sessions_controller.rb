dcf18b96258b4478619b0620cf95e1ab434aba76
James Reed <jamesreed.3050@gmail.com>
2023-03-03 22:59:33 +0900

    dealing with rails7/devise session bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  include RackSessionFix
 
   respond_to :json
 
   private
 
   def respond_with(resource, _opts = {})
+    if resource.persisted?
     render json: {
       status: {code: 200, message: 'Logged in sucessfully.'},
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
     }, status: :ok
+    else
+      render json: {
+        status: {message: "User couldn't be logged in successfully. #{resource.errors.full_messages.to_sentence}"}
+      }, status: :unprocessable_entity
+    end
   end
 
   def respond_to_on_destroy
