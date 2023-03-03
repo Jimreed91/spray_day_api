14242aca7eb1fe064c7512fc2dffd66b9def3cc2
James Reed <jamesreed.3050@gmail.com>
2023-03-03 12:38:45 +0900

    generating controllers for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Development setting for devise (change to host in production)
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -53,6 +56,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.navigational_formats = []
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
